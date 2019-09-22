1c565274787d58cb22a6307a20e9cf09dd114a83
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-09-21 18:29:10 -0700

    add index, new, and create action. Add book_params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,24 @@
 class BooksController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :genre)
+    end
+
 end
