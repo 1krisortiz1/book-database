5f4ee6f328be5dbe49c948ee5fd19be359b204c4
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-09-25 18:07:16 -0700

    add new, create, and destroy actions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,22 @@ class WelcomeController < ApplicationController
 
   end
 
+  def new
+  end
+
+  def create
+    @reader = Reader.find_by(username: params[:reader][:username])
+    if @reader.try(:authenticate, params[:reader][:password])
+      session[:reader_id] = @reader.id
+      redirect_to reader_path(@reader)
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.delete(:reader_id)
+    redirect_to '/'
+  end
+
 end
