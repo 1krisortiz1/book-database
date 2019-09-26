c19d61fb453d9ae23d9813c242ec36326c8f10c1
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-09-25 18:03:03 -0700

    add index, create, show, and private method

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -1,2 +1,30 @@
 class ReadersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @reader = Reader.new
+  end
+
+  def create
+    @reader = Reader.new(reader_params)
+    if @reader.save
+      session[:reader_id] = @reader.id
+      redirect_to reader_path(@reader)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @reader = Reader.find_by_id(params[:id])
+  end
+
+  private
+
+  def reader_params
+    params.require(:reader).permit(:username, :password)
+  end
 end
