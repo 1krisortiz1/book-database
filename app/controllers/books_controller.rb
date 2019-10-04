e830728a7d44edaab36a8b0b1cfd3ab7784c0508
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-10-04 12:07:03 -0700

    git status

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,18 @@
 class BooksController < ApplicationController
 
   def index
+    @book = Book.all
+
   end
 
   def new
     @book = Book.new
+    @book.build_author
   end
 
   def create
     @book = Book.new(book_params)
+    @book.reader_id = session[:reader_id]
     if @book.save
       redirect_to book_path(@book)
     else
@@ -17,12 +21,13 @@ class BooksController < ApplicationController
   end
 
   def show
-    #@book = Book.find(params[:id])
+    @book = Book.find(params[:id])
+    redirect_to books_path if !@book
   end
 
   private
     def book_params
-      params.require(:book).permit(:title, :genre, :author_id, :author_attributes: [:name])
+      params.require(:book).permit(:title, :genre, :author_id, author_attributes: [:name])
     end
 
 
