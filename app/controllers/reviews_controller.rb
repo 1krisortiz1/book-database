9d18aeb13b45f5ab4221986d3dea717b95ef524e
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-10-05 20:33:10 -0700

    Add reviews actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,38 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @book = Book.find_by_id(params[:book_id])
+    @review = @book.reviews.build
+    
+  end
+
+  def index
+    if @book = Book.find_by_id(params[:book_id])
+      @reviews = @book.reviews 
+    else
+      @reviews = Review.all
+    end
+  end
+
+  def create
+    @review = current_reader.reviews.build(review_params)
+    if @review.valid?
+      @review.save
+      redirect_to review_path(@review)
+    else
+      render :new
+    end
+
+  end
+
+  def show
+    @review = Review.find_by_id(params[:id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:stars, :title, :book_id, :content)
+  end
+
 end
